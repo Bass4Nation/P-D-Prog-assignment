d02044470d78c5b19961a3cd0a56a983f949c141
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-04-20 14:51:07 +0200

    Project 6 created

---
+++ project6/project-6/src/main/java/no/hiof/itf23019/project6/Main.java
@@ -0,0 +1,9 @@
+package no.hiof.itf23019.project6;
+
+public class Main
+{
+    public static void main( String[] args ) {
+        System.out.println("test");
+    }
+   
+}
---
+++ project6/project-6/src/main/java/no/hiof/itf23019/project6/parallel/MinMaxParallel.java
@@ -0,0 +1,4 @@
+package no.hiof.itf23019.project6.parallel;
+
+public class MinMaxParallel {
+}
